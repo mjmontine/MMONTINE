658c5c42aa7042695237a0316bb4d8db541294a2
mjmontine <69216019+mjmontine@users.noreply.github.com>
2020-08-04 13:44:24 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-#! /bin/bash
-echo "Hello $USER"
-echo "Hey i am" $USER "and will be telling you about the current processes"
-echo "Running processes List"
-ps
+chmod +x consumer_complaints.py
+python consumer_complaints.py
