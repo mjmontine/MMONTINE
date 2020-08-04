fa6f3fe42f8f30592d498427f467c7383d390a81
mjmontine <69216019+mjmontine@users.noreply.github.com>
2020-08-04 14:27:03 -0700

    Update consumer_complaints.py

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,2 +1,18 @@
 #Test Python Script
-print("Please God, don't let me embarrass myself in front of Denise")
+#print("Please God, don't let me embarrass myself in front of Denise")
+
+import csv
+
+fields = ['Name', 'Department']
+
+rows = [ ['Mark', 'Records'],
+         ['Macy', 'Collections'] ]
+
+filename = "name_department.csv"
+
+with open(filename, 'w') as csvfile:
+  csvwriter = csv.writer(csvfile)
+
+  csvwriter.writerow(fields)
+
+  csvwriter.writerows(rows)
