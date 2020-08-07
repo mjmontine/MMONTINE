3f5be6a88afffc2ba87c11ec72b53e04a2d70573
mjmontine <69216019+mjmontine@users.noreply.github.com>
2020-08-06 18:16:32 -0700

    Update consumer_complaints.py

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -6,7 +6,7 @@ import sys, os
 sys.path.append(os.path.realpath('..'))
 
 def write_temp_csv():
-    with open(os.path.realpath('../input/complaints.csv'), encoding='utf8') as complaints:
+    with open(os.path.realpath('../MMONTINE/input/complaints.csv'), encoding='utf8') as complaints:
         reader= csv.reader(complaints)
         for row in reader:
             included_cols = [1, 0, 7, 2]
@@ -44,7 +44,7 @@ with open('csvtemp.csv', 'r') as fin:
     fin.close
 
 def write_output_csv():
-    with open(os.path.realpath('../output/report.csv'), 'w+') as output_csv:
+    with open(os.path.realpath('../MMONTINE/output/report.csv'), 'w+') as output_csv:
         wr = csv.writer(output_csv)
         wr.writerow(['product', 'year', 'total complaints', '# of companies recieving >=1 complaint', 'highest % of complaints against 1 company'])
         for i in range(0, count_distinct_p_y[0]):
